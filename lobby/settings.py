345e0bd150000ef50cce187e8bc8acee616cba63
Brad Parker <cbparker@gmail.com>
2017-08-11 17:44:55 +0100

    add lobby.libretro.com to allowed hosts

--- lobby/settings.py
+++ lobby/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['newlobby.libretro.com']
+ALLOWED_HOSTS = ['newlobby.libretro.com', 'lobby.libretro.com']
 
 # Application definition
 
