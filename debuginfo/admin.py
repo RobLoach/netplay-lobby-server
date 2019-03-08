b548bda2d622cd58616a2328eb56837fef8415fc
Brad Parker <cbparker@gmail.com>
2019-03-08 00:16:22 +0000

    debuginfo: show ID number on admin changelist

--- debuginfo/admin.py
+++ debuginfo/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from models import *
 
 class DebugInfoAdmin(admin.ModelAdmin):
-  list_display = ['ip', 'created']
+  list_display = ['id', 'ip', 'created']
 
   def get_queryset(self, request):
     qs = super(DebugInfoAdmin, self).get_queryset(request)
